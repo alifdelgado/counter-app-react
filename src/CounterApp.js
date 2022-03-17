90ef70587d65d3d59501022ad5dcda25c98d6e54
Alif Delgado <alifdelgado@gmail.com>
2022-03-17 01:15:36 -0600

    First test added

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,10 +1,28 @@
 import { PropTypes } from "prop-types";
-const CounterApp = ({ value }) => (
-  <>
-    <h1>CounterApp</h1>
-    <h2>{value}</h2>
-  </>
-);
+import { useState } from "react";
+const CounterApp = ({ value = 10 }) => {
+  const [counter, setCounter] = useState(0);
+  const handledAdd = () => {
+    setCounter(counter + 1);
+  };
+
+  const handledReset = () => {
+    setCounter(value);
+  };
+
+  const handledSubstract = () => {
+    setCounter(counter - 1);
+  };
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+      <button onClick={handledAdd}>+1</button>
+      <button onClick={handledReset}>Reset</button>
+      <button onClick={handledSubstract}>-1</button>
+    </>
+  );
+};
 
 CounterApp.propTypes = {
   value: PropTypes.number,
